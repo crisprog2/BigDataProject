ea8824a7ab9f72c71fec95edb800bf190fc9ecb8
christian <christian@CHRISTIANPC>
2016-10-10 23:55:10 -0500

    prueba big data correcto

---
+++ BigDataProject/BigDataProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigDataProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int numpronombres = 0;
+            string prueba = "adonde";
+            string[] texto = System.IO.File.ReadAllLines(@"D:\IMPORTANTE_NO_BORRAR\DOCUMENTOS\BIG DATA\pronombres.txt", Encoding.UTF8);
+            foreach(string line in texto)
+            {
+                if (line.Equals(prueba))
+                {
+                    numpronombres++;
+                }
+            }
+            Console.WriteLine(numpronombres);
+            Console.ReadLine();*/
+            /*string valor = "hola mundo gonorrea";
+            Char delimitador = ' ';
+            string[] palabra = valor.Split(delimitador);
+            foreach(var palabras in palabra)
+            {
+                Console.WriteLine(palabras);
+            }*/
+            Console.ReadLine();
+        }
+    }
+}
